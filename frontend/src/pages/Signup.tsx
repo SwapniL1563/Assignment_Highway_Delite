64591450d203be679ecf9bc82938778f2d1d28ad
SwapniL1563 <choratswapnil15@gmail.com>
2025-07-31 15:31:56 +0530

    feat:add signup page with otp flow

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import SignUpForm from '../components/SignUpForm'
 
-const Signup = () => {
+const SignupPage = () => {
   return (
-    <div>Signup</div>
+    <div className='w-full min-h-screen flex p-2'>
+        <div className="flex flex-col w-full md:w-1/2 min-h-screen">
+
+        <div className="mb-8 p-2">
+          <img src="/logo.png" className="w-24" alt="Logo" />
+        </div>
+
+        <div className="flex-1 flex items-center justify-center">
+          <SignUpForm />
+        </div>
+        </div>
+
+        <div className="hidden md:block w-1/2 h-full">
+        <img src="/container.png" className="w-full min-h-screen object-cover" alt="Container" />
+        </div>
+    </div>
   )
 }
 
-export default Signup
+export default SignupPage
